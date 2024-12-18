4e601e7232d71c801a44a40f2dd34197c522dff3
Lucas de Azevedo <77306471+LucasAzoli@users.noreply.github.com>
2024-12-18 18:49:59 -0300

    Add test files
    A small test for 3d Gaussian renderer

---
+++ main.js
@@ -0,0 +1,18 @@
+import * as THREE from 'three';
+
+import * as GaussianSplats3D from './node_modules/@mkkellogg/gaussian-splats-3d/build/gaussian-splats-3d.module.js';
+
+const viewer = new GaussianSplats3D.Viewer({
+    'cameraUp': [0, -1, -.17],
+    'initialCameraPosition': [-5, -1, -1],
+    'initialCameraLookAt': [-1.72477, 0.05395, -0.00147],
+    'sphericalHarmonicsDegree': 2,
+    'sharedMemoryForWorkers': false
+});
+let path = 'assets/3dModels/truck/playroom.splat';
+viewer.addSplatScene(path, {
+  'progressiveLoad': false
+})
+.then(() => {
+    viewer.start();
+});
